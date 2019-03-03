3e82ad1c6d6cef01af5ecfa0ad846c08e70b3d20
Aleksa Stojanović <afidelo@protonmail.com>
2019-03-03 20:40:36 +0100

    components with properties 1/2

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
 
-	return <div> Hi there! :) </div>
+	const something = 'blabla';
+
+	return (
+		<div className='ui container comments'>
+			<CommentDetail
+			 	author="Sam"
+			 	timeAgo="Today at 4:20 PM"
+			 	content="Great blog!"
+			 	avatar={faker.image.avatar()} />
+			<CommentDetail 
+			 	author="Alexus"
+			 	timeAgo="Today at 4:21 PM" 
+			 	content="Nice article!"
+			 	avatar={faker.image.avatar()}/>
+			<CommentDetail
+				author="kleine Alexus"
+				timeAgo="Yesterday at 5:00 PM"
+				content="I love this subject!"
+				avatar={faker.image.avatar()}/>
+			<CommentDetail 
+				author="Jane" 
+				timeAgo="Today at 4:44 PM" 
+				content="I'll start to research"
+				avatar={faker.image.avatar()}/>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
